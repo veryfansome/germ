4e4976c0b5a7760c4173951bd1b602411180fc93
fzhu <fzhu@yahooinc.com>
2024-08-05 14:26:36 -0700

    trying mounting /src as rw

--- ml/starterkit/activations/image_model.py
+++ ml/starterkit/activations/image_model.py
@@ -2,8 +2,13 @@ from api.models import ChatMessage
 from ml.activations.training import ActivationTrainingExample
 
 EXAMPLES = [
-    ActivationTrainingExample(labels={"dall-e-2": "off", "dall-e-3": "off"}, messages=[
-        ChatMessage(role="Hi", conent=""),
+    ActivationTrainingExample(labels={"dall-e-2": "on", "dall-e-3": "off"}, messages=[
+        ChatMessage(role="user", conent="Generate an image of a dog"),
+    ]),
+    ActivationTrainingExample(labels={"dall-e-2": "on", "dall-e-3": "off"}, messages=[
+        ChatMessage(role="user", conent="hi"),
+        ChatMessage(role="assistant", conent="Hello! How can I assist you today?"),
+        ChatMessage(role="user", conent="generate an image of a house"),
     ]),
 ]
 
@@ -20,6 +25,6 @@ if __name__ == "__main__":
     activators = {}
     for image_model_name in ENABLED_IMAGE_MODELS_FOR_TRAINING_DATA_CAPTURE:
         activators[image_model_name] = new_activation_predictor(
-            image_model_name, model_dir="")
+            image_model_name, model_dir=germ_settings.STARTERKIT_DIR)
     trainer = ActivationTrainer(EXAMPLES, activators, germ_settings.IMAGE_MODEL_STARTERKIT_TRAINING_ROUNDS)
     trainer.train()
