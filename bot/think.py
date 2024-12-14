a6eb293493de9e5d9aa8a8be6ee824ecb9f54fb8
fzhu <fzhu@yahooinc.com>
2024-12-14 13:53:52 -0800

    feat: more structured thinker output data format

--- bot/think.py
+++ bot/think.py
@@ -11,7 +11,6 @@ from starlette.concurrency import run_in_threadpool
 
 from api.models import ChatMessage
 from observability.logging import logging, setup_logging
-from settings.openai_settings import HTTPX_TIMEOUT
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +27,7 @@ class DelegatingThinker(ABC):
         pass
 
     @abstractmethod
-    def reset(self):
+    def reset(self, history: list[ChatMessage] = None):
         pass
 
     @abstractmethod
@@ -41,14 +40,14 @@ class DelegatingThinker(ABC):
 
 
 class PairedThinker(DelegatingThinker):
-    def __init__(self, model="gpt-4o-mini", name: str = "", pair: DelegatingThinker = None,
-                 history: list[ChatMessage] = None,
-                 system_message: str = "Be thoughtful."):
+    def __init__(self, name: str = "", pair: DelegatingThinker = None,
+                 history: list[ChatMessage] = None, system_message: str = "Be thoughtful."):
         self._name = name
         self._pair: Optional[DelegatingThinker] = pair
         self._stop = False
         self.history: list[ChatMessage] = history if history is not None else []
-        self.model = model
+        self.completion_model = "gpt-4o-mini"
+        self.summarization_model = "gpt-4o"
         self.system_message = system_message
 
     async def delegate(self, message: str,
@@ -62,19 +61,19 @@ class PairedThinker(DelegatingThinker):
         logger.info(f"{self._name}[hist:{len(self.history)}] received: {message}")
 
         initial_call_task = None
-        if initial_call and self._pair is not None:
+        if initial_call and not self._stop and self._pair is not None:
             # Let pair have a go too so both see the initial message.
             # Don't delegate because we'll delegate later, and it would create two loops.
             initial_call_task = asyncio.create_task(self._pair.delegate(message, delegate_ok=False))
 
         self.history.append(ChatMessage(role="user", content=message))
-
-        next_message = await run_in_threadpool(self.do_completion)
-        if delegate_ok:
-            if initial_call_task is not None:
-                await initial_call_task
-            if not self._stop and self._pair is not None:
-                delegate_task = asyncio.create_task(self._pair.delegate(next_message))
+        if not self._stop:
+            next_message = await run_in_threadpool(self.do_completion)
+            if delegate_ok:
+                if initial_call_task is not None:
+                    await initial_call_task
+                if not self._stop and self._pair is not None:  # Check stop in case we waited
+                    delegate_task = asyncio.create_task(self._pair.delegate(next_message))
 
     def do_completion(self):
         with OpenAI() as client:
@@ -89,9 +88,9 @@ class PairedThinker(DelegatingThinker):
                     },
                     *self.history,
                 ],
-                model=self.model,
+                model=self.completion_model,
                 n=1, temperature=1,
-                timeout=HTTPX_TIMEOUT)
+                timeout=30)
             self.history.append(ChatMessage(role="assistant", content=completion.choices[0].message.content))
             return completion.choices[0].message.content
 
@@ -104,9 +103,11 @@ class PairedThinker(DelegatingThinker):
     def pair_summarize(self) -> str:
         return self._pair.summarize()
 
-    def reset(self):
-        self.history = []
-        self._pair.reset()
+    def pair_reset(self, history: list[ChatMessage] = None) -> str:
+        return self._pair.reset(history)
+
+    def reset(self, history: list[ChatMessage] = None):
+        self.history = history if history is not None else []
 
     async def ruminate(self, idea: str,
                        num_ticks: int = 3):  # Odds end on Self, evens end on Pair
@@ -168,18 +169,24 @@ class PairedThinker(DelegatingThinker):
                         "content": f"Update your initial take on \"{self.history[0].content}\"",
                     },
                 ],
-                model=self.model,
+                model=self.summarization_model,
                 n=1, temperature=1,
-                timeout=HTTPX_TIMEOUT)
+                timeout=60)
             return completion.choices[0].message.content
 
 
-DEVILS_ADVOCATE = "devils_advocate"
-SELF_INTEREST = "self_interest"
-UTILITARIAN = "utilitarian"
+DEVILS_ADVOCATE = "Devil's advocate"
+HEDONIST = "Hedonist"
+OPTIMIST = "Optimist"
+SELF_INTEREST = "Self interest"
+UTILITARIAN = "Greatest good"
 THINKERS = {
     DEVILS_ADVOCATE: PairedThinker(name=DEVILS_ADVOCATE,
                                    system_message="Be challenging. Play devil's advocate."),
+    HEDONIST: PairedThinker(name=HEDONIST,
+                            system_message="Be a hedonist. Seek pleasure."),
+    OPTIMIST: PairedThinker(name=OPTIMIST,
+                            system_message="Be optimistic. Go all in."),
     SELF_INTEREST: PairedThinker(name=SELF_INTEREST,
                                  system_message="Be greedy. Champion self interest."),
     UTILITARIAN: PairedThinker(name=UTILITARIAN,
@@ -187,16 +194,19 @@ THINKERS = {
 }
 
 
-async def cross_ruminate(idea: str, limiter: asyncio.Semaphore = asyncio.Semaphore(1)):
+async def cross_ruminate(idea: str,
+                         thinkers: dict[str, PairedThinker] = None,
+                         limiter: asyncio.Semaphore = asyncio.Semaphore(1)):
     """
     Execute all possible ruminate combinations, with each pair having distinct histories, then distill
     the arguments from the discussions.
 
     :param idea:
+    :param thinkers:
     :param limiter:
     :return:
     """
-    summaries = {k: [] for k in THINKERS.keys()}  # Allows grouping by thinker type
+    summaries = {k: [] for k in (THINKERS if thinkers is None else thinkers).keys()}  # Allows grouping by thinker type
     lead_thinkers: [PairedThinker] = []
     for p1, p2 in list(itertools.combinations(THINKERS.values(), 2)):  # All combos
         # Copy or histories will be jumbled
@@ -222,22 +232,22 @@ async def cross_ruminate(idea: str, limiter: asyncio.Semaphore = asyncio.Semapho
         summaries[thinker.pair_name()].append(thinker.pair_summarize())
     # Organized by thinker types
     for thinker_name in summaries.keys():
-        blobs.append(f"## {thinker_name}\n" + (''.join([f'- {s}\n' for s in summaries[thinker_name]])))
+        blobs.append(f"## Thinker: {thinker_name}\n" + (''.join([f'- {s}\n' for s in summaries[thinker_name]])))
     with OpenAI() as client:
         completion = client.chat.completions.create(
             messages=[
                 {
                     "role": "system",
-                    "content": "Retain each distinct thought while dropping duplicative text",
+                    "content": "Retain distinct thoughts. Condense duplicative text.",
                 },
                 {
                     "role": "user",
                     "content": "# Distill these thoughts\n---\n" + ("\n".join(blobs)),
                 },
             ],
-            model="gpt-4o-mini",
+            model="gpt-4o",
             n=1, temperature=1,
-            timeout=HTTPX_TIMEOUT,
+            timeout=120,
             tool_choice={
                 "type": "function",
                 "function": {"name": "format_thoughts"}
@@ -246,14 +256,28 @@ async def cross_ruminate(idea: str, limiter: asyncio.Semaphore = asyncio.Semapho
                 "type": "function",
                 "function": {
                     "name": "format_thoughts",
-                    "description": "Format thoughts for user presentation",
+                    "description": "Format thoughts for user presentation.",
                     "parameters": {
                         "type": "object",
                         "properties": {
                             "thoughts": {
                                 "type": "array",
+                                "description": "Distilled thoughts.",
                                 "items": {
-                                    "type": "string"
+                                    "type": "object",
+                                    "properties": {
+                                        "thought": {
+                                            "type": "string",
+                                            "description": "Distilled thought."
+                                        },
+                                        "thinkers": {
+                                            "type": "array",
+                                            "items": {
+                                                "type": "string",
+                                                "description": "Thinker."
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         },
@@ -266,18 +290,33 @@ async def cross_ruminate(idea: str, limiter: asyncio.Semaphore = asyncio.Semapho
         return json.loads(completion.choices[0].message.tool_calls[0].function.arguments)
 
 
-async def quick_take(idea: str):
+async def forum(idea: str, thinkers: dict[str, PairedThinker] = None,):
+    history: list[ChatMessage] = []
+    thinkers = [copy.deepcopy(t) for t in (THINKERS if thinkers is None else thinkers).values()]
+    for thinker in thinkers:
+        thinker.reset(history=history)
+
+
+async def ruminate_randomly(idea: str, thinkers: dict[str, PairedThinker] = None):
     """
     Ruminate once using a randomized thinker pair.
 
+    :param thinkers:
     :param idea:
     :return:
     """
-    thinkers = [copy.deepcopy(t) for t in random.sample(list(THINKERS.values()), 2)]
+    thinkers = [copy.deepcopy(t) for t in random.sample(list((THINKERS if thinkers is None else thinkers).values()), 2)]
     thinkers[0].set_pair(thinkers[1])
     thinkers[1].set_pair(thinkers[0])
     thinker = await thinkers[0].ruminate_randomly(idea)
-    return {"thoughts": [thinker.summarize(), thinker.pair_summarize()]}
+    return {
+        "thoughts": [{
+            "thought": thinker.summarize(),
+            "thinkers": [thinker.name()],
+        }, {
+            "thought": thinker.pair_summarize(),
+            "thinkers": [thinker.pair_name()],
+        }]}
 
 
 if __name__ == '__main__':
@@ -285,14 +324,18 @@ if __name__ == '__main__':
 
     setup_logging(global_level="INFO")
     parser = argparse.ArgumentParser(description='Think about an idea.')
-    parser.add_argument("-i", "--idea", help='Any idea, simple or complex.',
-                        default="People are good.")
     parser.add_argument("--cross", action="store_true", help='Cross-ruminate.',
                         default=False)
+    parser.add_argument("--forum", action="store_true", help='Conduct a forum.',
+                        default=False)
+    parser.add_argument("-i", "--idea", help='Any idea, simple or complex.',
+                        default="People are good.")
     args = parser.parse_args()
 
     if args.cross:
         semaphore = asyncio.Semaphore(2)
         print(json.dumps(asyncio.run(cross_ruminate(args.idea, limiter=semaphore)), indent=4))
+    elif args.forum:
+        print(asyncio.run(forum(args.idea)))
     else:
-        print(json.dumps(asyncio.run(quick_take(args.idea)), indent=4))
+        print(json.dumps(asyncio.run(ruminate_randomly(args.idea)), indent=4))
