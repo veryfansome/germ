5afb15e56af384b10480a9cc313c0e802e4556d1
fzhu <fzhu@yahooinc.com>
2024-12-17 12:32:09 -0800

    feat: more robust openai feature extraction

--- bot/lang/utils.py
+++ bot/lang/utils.py
@@ -1,12 +1,24 @@
-import re
+import difflib
 from flair.data import Sentence
 from flair.models import SequenceTagger
+from inflect import engine as inflect_engine
 from openai import OpenAI
+from textblob import TextBlob
 
-from observability.logging import setup_logging
+from observability.logging import logging, setup_logging
 
+logger = logging.getLogger(__name__)
+
+differ = difflib.Differ()
+inf_engine = inflect_engine()
 ner_tagger = SequenceTagger.load("ner")
 pos_tagger = SequenceTagger.load("pos")
+
+
+def diff_strings(str1, str2):
+    result = list(differ.compare(str1.splitlines(), str2.splitlines()))
+    differences = [line for line in result if line.startswith('+ ') or line.startswith('- ')]
+    return differences
 
 
 def flair_text_feature_extraction(text: str):
@@ -16,8 +28,8 @@ def flair_text_feature_extraction(text: str):
     :param text:
     :return:
     """
-    proper_nouns = []
     pos_tags = []
+    proper_nouns = []
     verbs = []
     flair_sentence = Sentence(test)
     pos_tagger.predict(flair_sentence)
@@ -36,119 +48,159 @@ def flair_text_feature_extraction(text: str):
     }
 
 
-def openai_text_feature_extraction(text: str):
+def openai_text_feature_extraction(text: str,
+                                   json_to_check: str = None,
+                                   model: str = "gpt-4o-mini",
+                                   prefer_second_opinion: bool = False,
+                                   second_opinion: bool = False,
+                                   temperature: int = 0):
     """
-    Way better for emotionality (variety, intensity, transitions), sentiment, related knowledge areas, related topics.
+    Way better for emotionality (variety, intensity, transitions), sentiment, related knowledge areas, related topics,
+    and other contextual tasks.
 
+    Setting `second_opinion` to True can improve accuracy but is obviously more expensive.
+
+    :param json_to_check:
+    :param model:
+    :param prefer_second_opinion:
+    :param second_opinion:
+    :param temperature:
     :param text:
     :return:
     """
+    system_message = "Extract text features."
+    if json_to_check is not None:
+        system_message = "Check the extracted text features and correct errors."
+        text = f"# Text\n{text}\n\n# Features JSON\n{json_to_check}"
+
+    tool_name = "store_text_features"
+    tool_properties_spec = {
+        "emotions": {
+            "type": "array",
+            "description": "List of emotions.",
+            "items": {
+                "type": "string",
+                "description": "An emotion.",
+            }
+        },
+        "emotional_intensity": {
+            "type": "string",
+            "description": "Intensity of emotions.",
+            "enum": ["n/a", "low", "high"]
+        },
+        "emotional_nuance": {
+            "type": "string",
+            "description": "Complexity of emotions.",
+            "enum": ["n/a", "low", "high"]
+        },
+        "entities": {
+            "type": "array",
+            "description": "List of named entities.",
+            "items": {
+                "type": "object",
+                "description": "A named entity.",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "description": "Named entity name."
+                    },
+                    "type": {
+                        "type": "string",
+                        "description": "Named entity type."
+                    },
+                }
+            }
+        },
+        "knowledge": {
+            "type": "array",
+            "description": "List of applicable knowledge categories.",
+            "items": {
+                "type": "string",
+                "description": "A category."
+            }
+        },
+        "numerals": {
+            "type": "array",
+            "description": "List of numeric values.",
+            "items": {
+                "type": "object",
+                "description": "A numeric value.",
+                "properties": {
+                    "value": {
+                        "type": "string",
+                        "description": "Numeric value."
+                    },
+                    "type": {
+                        "type": "string",
+                        "description": "Type of numeric value."
+                    },
+                    "unit": {
+                        "type": "string",
+                        "description": "Unit of numeric value."
+                    },
+                }
+            }
+        },
+        "sentiment": {
+            "type": "string",
+            "description": "Sentiment of text.",
+            "enum": ["mixed", "negative", "neutral", "positive"]
+        },
+        "subjects": {
+            "type": "array",
+            "description": "List of sentence subjects.",
+            "items": {
+                "type": "string",
+                "description": "A subject."
+            }
+        },
+        "topics": {
+            "type": "array",
+            "description": "List of topics.",
+            "items": {
+                "type": "string",
+                "description": "A topic."
+            }
+        },
+    }
     with OpenAI() as client:
         completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Extract text features.",
-                },
-                {
-                    "role": "user",
-                    "content": text,
-                },
-            ],
-            model="gpt-4o-mini",
-            n=1, temperature=0,
-            timeout=60,
-            tool_choice={
-                "type": "function",
-                "function": {"name": "store_text_features"}
-            },
+            messages=[{"role": "system", "content": system_message}, {"role": "user", "content": text}],
+            model=model, n=1, temperature=temperature, timeout=60,
+            tool_choice={"type": "function", "function": {"name": tool_name}},
             tools=[{
                 "type": "function",
                 "function": {
-                    "name": "store_text_features",
+                    "name": tool_name,
                     "description": "Store extracted text features.",
                     "parameters": {
                         "type": "object",
                         "description": "Features extracted from text.",
-                        "properties": {
-                            "emotions": {
-                                "type": "array",
-                                "description": "List of emotions.",
-                                "items": {
-                                    "type": "string",
-                                    "description": "An emotion.",
-                                }
-                            },
-                            "emotional_intensity": {
-                                "type": "string",
-                                "enum": ["low", "high"]
-                            },
-                            "entities": {
-                                "type": "array",
-                                "description": "List of named entities.",
-                                "items": {
-                                    "type": "object",
-                                    "description": "A named entity.",
-                                    "properties": {
-                                        "name": {
-                                            "type": "string",
-                                            "description": "Named entity name."
-                                        },
-                                        "type": {
-                                            "type": "string",
-                                            "description": "Named entity type."
-                                        },
-                                    }
-                                }
-                            },
-                            "knowledge": {
-                                "type": "array",
-                                "description": "List of applicable knowledge categories.",
-                                "items": {
-                                    "type": "string",
-                                    "description": "A category."
-                                }
-                            },
-                            "sentiment": {
-                                "type": "string",
-                                "description": "Sentiment of text.",
-                                "enum": ["mixed", "negative", "neutral", "positive"]
-                            },
-                            "subjects": {
-                                "type": "array",
-                                "description": "List of sentence subjects.",
-                                "items": {
-                                    "type": "string",
-                                    "description": "A subject."
-                                }
-                            },
-                            "topics": {
-                                "type": "array",
-                                "description": "List of topics.",
-                                "items": {
-                                    "type": "string",
-                                    "description": "A topic."
-                                }
-                            },
-                        },
-                        "required": [
-                            "emotions", "emotional_intensity", "entities", "knowledge",
-                            "sentiment", "subjects", "topics",
-                        ],
+                        "properties": tool_properties_spec,
+                        "required": list(tool_properties_spec.keys()),
                         "additionalProperties": False,
                     },
                 }
             }]
         )
-        return completion.choices[0].message.tool_calls[0].function.arguments
+        new_feature_json = completion.choices[0].message.tool_calls[0].function.arguments
+        if second_opinion:
+            return openai_text_feature_extraction(
+                text, json_to_check=new_feature_json, prefer_second_opinion=prefer_second_opinion)
 
-
-def re_findall_numerals(sentence: str) -> list[str]:
-    return re.findall(r'\d+', sentence)
+        if json_to_check is None:
+            return new_feature_json
+        else:
+            diffs = diff_strings(json_to_check, new_feature_json)
+            if diffs:
+                logger.warning("diffs on second opinion:\n" + '\n'.join(diffs))
+                if prefer_second_opinion:
+                    return new_feature_json
+            return json_to_check
 
 
 if __name__ == '__main__':
-    setup_logging(global_level="ERROR")
+    setup_logging(global_level="INFO")
 
     test_set = [
         "Hello, world!",
@@ -173,6 +225,5 @@ if __name__ == '__main__':
 Sentence: {test}
 ---
 flair_feature_extraction: {flair_text_feature_extraction(test)}
-openai_text_feature_extraction: {openai_text_feature_extraction(test)}
-re_findall_numerals: {re_findall_numerals(test)}
+openai_text_feature_extraction: {openai_text_feature_extraction(test, second_opinion=True)}
 """)
