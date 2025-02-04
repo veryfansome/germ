aa8c7e1c271e7ee651efedbf9f5207b24552e0e5
fzhu <fzhu@yahooinc.com>
2025-02-03 21:28:25 -0800

    chore: simplified settings

--- settings/germ_settings.py
+++ settings/germ_settings.py
@@ -3,17 +3,8 @@ import uuid
 
 # Service
 
-SERVICE_NAME = os.getenv("GERM_SERVICE_NAME", "germ-bot")
-WEBSOCKET_CONNECTION_IDLE_TIMEOUT = 3600
-
 UUID5_NS = uuid.UUID("246a5463-afae-4571-a6e0-f319d74147d3")  # Changes sentences signatures
-
-# Model training
-
-DEFAULT_BERT_MODEL = os.getenv("GERM_DEFAULT_BERT_MODEL", "distilbert-base-cased")
-IMAGE_MODEL_STARTERKIT_TRAINING_ROUNDS = os.getenv("GERM_IMAGE_MODEL_STARTERKIT_TRAINING_ROUNDS", 50)
-MODEL_DIR = os.getenv("GERM_MODEL_DIR", "/var/lib/germ/models")
-STARTERKIT_DIR = os.getenv("GERM_STARTERKIT_DIR", "/src/data/germ/starterkit")
+WEBSOCKET_CONNECTION_IDLE_TIMEOUT = 3600
 
 # Observability
 
