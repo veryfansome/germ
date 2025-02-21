e66f1dfd435ee4997c6682766be145b366966bf8
fzhu <fzhu@yahooinc.com>
2025-02-21 00:21:57 -0800

    feat: germ to models token classification call

--- bot/chat/controller.py
+++ bot/chat/controller.py
@@ -1,7 +1,10 @@
+from starlette.concurrency import run_in_threadpool
+import aiohttp
 import asyncio
 import logging
 
-from bot.api.models import ChatRequest
+from bot.api.models import ChatRequest, TextPayload
+from bot.lang.parsers import extract_markdown_page_elements
 from bot.websocket import WebSocketReceiveEventHandler, WebSocketSender
 from observability.annotations import measure_exec_seconds
 
@@ -19,6 +22,20 @@ class ChatController(WebSocketReceiveEventHandler):
         remote_response_task = asyncio.create_task(
             self.remote.on_receive(chat_session_id, chat_request_received_id, chat_request, ws_sender),
         )
-        # TODO: implement local completions
+        # TODO:
+        #  - implement local completions
+        #  - maybe even user profiling should be moved here...
+        elements = await run_in_threadpool(extract_markdown_page_elements, chat_request.messages[-1].content)
+        for element in elements:
+            if element[0] == "paragraph":
+                async with aiohttp.ClientSession() as session:
+                    async with session.post("http://germ-models:9000/text/classification",
+                                            json={"text": element[1]}) as response:
+                        data = await response.json()
+                        logger.info("token classifications:\n" + (
+                            "\n".join([f"{head}\t{labels}" for head, labels in data.items()])))
+            else:
+                logger.info(f"ignored element type: {element[0]}")
+                continue
 
         await remote_response_task
