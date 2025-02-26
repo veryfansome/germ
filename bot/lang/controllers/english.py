25c5ef404d5b1935ba34daa1c7c8f87547617bdf
fzhu <fzhu@yahooinc.com>
2025-02-25 22:57:40 -0800

    feat: label groups in logs

--- bot/lang/controllers/english.py
+++ bot/lang/controllers/english.py
@@ -61,6 +61,69 @@ class EnglishController(CodeBlockMergeEventHandler, ParagraphMergeEventHandler, 
             logger.info(f"on_periodic_run: sentence_id={sentence_id}\nopenai={sentence_parameters}\n" + (
                 "\n".join([f"{head}\t{labels}" for head, labels in multi_head_labels.items()])))
 
+            add_positions = extract_label_idx_groups(
+                multi_head_labels["misc"], {"ADD"})
+            logger.info(f"add positions: {idx_group_to_labels(add_positions, multi_head_labels)}")
+
+            cd_positions = extract_label_idx_groups(
+                multi_head_labels["misc"], {"CD"})
+            logger.info(f"cd positions: {idx_group_to_labels(cd_positions, multi_head_labels)}")
+
+            det_positions = extract_label_idx_groups(
+                multi_head_labels["det"], {"DT", "PDT"})
+            logger.info(f"determiner positions: {idx_group_to_labels(det_positions, multi_head_labels)}")
+            # DT NN .. determiner right before noun
+
+            event_positions = extract_label_idx_groups(
+                multi_head_labels["ner2"], {"B-EVENT", "I-EVENT"})
+            logger.info(f"event positions: {idx_group_to_labels(event_positions, multi_head_labels)}")
+
+            func_positions = extract_label_idx_groups(
+                multi_head_labels["func"], {"CC", "IN", "RP", "TO"})
+            logger.info(f"func positions: {idx_group_to_labels(func_positions, multi_head_labels)}")
+
+            loc_positions = extract_label_idx_groups(
+                multi_head_labels["ner2"], {"B-LOC", "I-LOC"})
+            logger.info(f"loc positions: {idx_group_to_labels(loc_positions, multi_head_labels)}")
+
+            noun_positions = extract_label_idx_groups(
+                multi_head_labels["noun"], {"NN", "NNS", "NNP", "NNPS"})
+            logger.info(f"noun positions: {idx_group_to_labels(noun_positions, multi_head_labels)}")
+
+            org_positions = extract_label_idx_groups(
+                multi_head_labels["ner1"], {"B-ORG", "I-ORG"})
+            logger.info(f"org positions: {idx_group_to_labels(org_positions, multi_head_labels)}")
+
+            person_positions = extract_label_idx_groups(
+                multi_head_labels["ner1"], {"B-PER", "I-PER"})
+            logger.info(f"person positions: {idx_group_to_labels(person_positions, multi_head_labels)}")
+
+            pronoun_positions = extract_label_idx_groups(
+                multi_head_labels["pronoun"], {"POS", "PRP$", "PRP"})
+            logger.info(f"pronoun positions: {idx_group_to_labels(pronoun_positions, multi_head_labels)}")
+
+            quest_positions = extract_label_idx_groups(
+                multi_head_labels["punct"], {"QUESTION"})
+            logger.info(f"question mark positions: {idx_group_to_labels(quest_positions, multi_head_labels)}")
+
+            modal_positions = extract_label_idx_groups(
+                multi_head_labels["verb"], {"MD"})
+            logger.info(f"modal positions: {idx_group_to_labels(modal_positions, multi_head_labels)}")
+
+            time_positions = extract_label_idx_groups(
+                multi_head_labels["misc"], {"TIME"})
+            logger.info(f"time positions: {idx_group_to_labels(time_positions, multi_head_labels)}")
+
+            verb_positions = extract_label_idx_groups(
+                multi_head_labels["verb"], {"VB", "VBD", "VBG", "VBN", "VBP", "VBZ"})
+            logger.info(f"verb positions: {idx_group_to_labels(verb_positions, multi_head_labels)}")
+            # ^VB PRP ... might be an imperative if PRP is me?
+
+            wh_positions = extract_label_idx_groups(
+                multi_head_labels["wh"], {"WDT", "WP$", "WP", "WRB"})
+            logger.info(f"wh positions: {idx_group_to_labels(wh_positions, multi_head_labels)}")
+            # might be interrogative
+
     async def on_code_block_merge(self, code_block: str, code_block_id: int):
         logger.info(f"on_code_block_merge: code_block_id={code_block_id}, {code_block}")
 
@@ -114,8 +177,56 @@ class EnglishController(CodeBlockMergeEventHandler, ParagraphMergeEventHandler, 
         self.unlabeled_sentences.append((sentence, sentence_id, sentence_parameters))
 
 
+def extract_label_idx_groups(labels, target_labels):
+    """
+    For example, given a list of labels (e.g. ["O", "O", "NN", "NN", "O", "O", "NNS", "O"]),
+    this function will extract the index positions of the labels: NN, NNS, NNP, NNPS.
+
+    It returns a list of consecutive index groupings for those noun labels.
+    For example:
+        ["O", "O", "NN", "NN", "O", "O", "NNS", "O"]
+    would return:
+        [[2, 3], [6]]
+
+    Args:
+        labels (list of str): The list of labels.
+        target_labels (set of str): The set of tags to target.
+
+    Returns:
+        list of lists of int: A list where each sub-list contains consecutive indices
+                              of labels that match NN, NNS, NNP, NNPS.
+    """
+    groups = []
+    current_group = []
+
+    for idx, label in enumerate(labels):
+        if label in target_labels:
+            # If current_group is empty or the current idx is consecutive (i.e., previous index + 1),
+            # append to current_group. Otherwise, start a new group.
+            if current_group and idx == current_group[-1] + 1:
+                current_group.append(idx)
+            else:
+                if current_group:
+                    groups.append(current_group)
+                current_group = [idx]
+        else:
+            if current_group:
+                groups.append(current_group)
+                current_group = []
+
+    # If there's an open group at the end, add it
+    if current_group:
+        groups.append(current_group)
+
+    return groups
+
+
 async def get_token_classifications(text: str):
     async with aiohttp.ClientSession() as session:
         async with session.post("http://germ-models:9000/text/classification",
                                 json={"text": text}) as response:
             return await response.json()
+
+
+def idx_group_to_labels(idx_groups, all_labels):
+    return [" ".join([all_labels["tokens"][i] for i in g]) for g in idx_groups]
