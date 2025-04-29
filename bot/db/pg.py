bd095e3551f4f81c022aeb52d59baf406a9afb94
veryfansome <135667604+veryfansome@users.noreply.github.com>
2025-04-28 21:52:20 -0700

    chore: engine in main.py + pg.py with TableHelper (#77)
    Co-authored-by: fzhu <fzhu@yahooinc.com>

---
+++ bot/db/pg.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Engine, MetaData, Table
+
+from settings import germ_settings
+
+DATABASE_URL = "{name}:{password}@{host}/germ".format(
+    host=germ_settings.DB_HOST,
+    name=germ_settings.POSTGRES_USER,
+    password=germ_settings.POSTGRES_PASSWORD,
+)
+
+
+class TableHelper:
+    def __init__(self, engine: Engine):
+        self.chat_user_table = Table('chat_user', MetaData(), autoload_with=engine)
+        self.chat_session_table = Table('chat_session', MetaData(), autoload_with=engine)
+        self.chat_message_table = Table('chat_message', MetaData(), autoload_with=engine)
