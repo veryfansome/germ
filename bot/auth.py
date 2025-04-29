e2d6956b7b906732829a0e6b291a10deb16dcbf7
Fan Zhu <fzhu.admin@icloud.com>
2025-04-29 00:06:00 -0700

    feat: DDL cutover

--- bot/auth.py
+++ bot/auth.py
@@ -1,53 +1,55 @@
-from sqlalchemy import MetaData, Table, insert, select
+from sqlalchemy import Table, insert, select
+from sqlalchemy.ext.asyncio import async_sessionmaker as async_pg_session_maker
 import bcrypt
 import logging
-
-from bot.db.models import AsyncSessionLocal, engine
 
 logger = logging.getLogger(__name__)
 
 SESSION_COOKIE_NAME = "ssid"
 MAX_COOKIE_AGE: int = 60 * 60 * 24 * 7   # 7 days
 
-chat_user_table = Table('chat_user', MetaData(), autoload_with=engine)
+
+class AuthHelper:
+    def __init__(self, chat_user_table: Table, pg_session_maker: async_pg_session_maker):
+        self.pg_session_maker = pg_session_maker
+        self.chat_user_table = chat_user_table
+
+    async def add_new_user(self, username: str, plain_text_password: str) -> int | None:
+        async with (self.pg_session_maker() as rdb_session):
+            async with rdb_session.begin():
+                try:
+                    insert_stmt = insert(self.chat_user_table).values(
+                        user_name=username,
+                        password_hash=hash_password(plain_text_password),
+                    ).returning(self.chat_user_table.c.user_id)
+                    result = await rdb_session.execute(insert_stmt)
+                    await rdb_session.commit()
+                    user_id = result.scalar()
+                    logger.info(f"New user '{username}' inserted successfully with user_id {user_id}")
+                    return user_id
+                except Exception as e:
+                    logger.error(f"Failed to insert new user {username}: {e}")
+                    await rdb_session.rollback()
+                    return None
 
 
-async def add_new_user(username: str, plain_text_password: str) -> int | None:
-    async with (AsyncSessionLocal() as rdb_session):
-        async with rdb_session.begin():
-            try:
-                insert_stmt = insert(chat_user_table).values(
-                    user_name=username,
-                    password_hash=hash_password(plain_text_password),
-                ).returning(chat_user_table.c.user_id)
-                result = await rdb_session.execute(insert_stmt)
-                await rdb_session.commit()
-                user_id = result.scalar()
-                logger.info(f"New user '{username}' inserted successfully with user_id {user_id}")
-                return user_id
-            except Exception as e:
-                logger.error(f"Failed to insert new user {username}: {e}")
-                await rdb_session.rollback()
-                return None
+    async def get_single_chat_user_attr_by_username(self, username: str, attr):
+        async with (self.pg_session_maker() as rdb_session):
+            async with rdb_session.begin():
+                chat_user_stmt = select(attr).where(
+                    self.chat_user_table.c.user_name == username
+                )
+                record = (await rdb_session.execute(chat_user_stmt)).one_or_none()
+                if record is not None:
+                    return record[0]
 
 
-async def get_single_chat_user_attr_by_username(username: str, attr):
-    async with (AsyncSessionLocal() as rdb_session):
-        async with rdb_session.begin():
-            chat_user_stmt = select(attr).where(
-                chat_user_table.c.user_name == username
-            )
-            record = (await rdb_session.execute(chat_user_stmt)).one_or_none()
-            if record is not None:
-                return record[0]
+    async def get_user_id(self, username: str):
+        return await self.get_single_chat_user_attr_by_username(username, self.chat_user_table.c.user_id)
 
 
-async def get_user_id(username: str):
-    return await get_single_chat_user_attr_by_username(username, chat_user_table.c.user_id)
-
-
-async def get_user_password_hash(username: str):
-    return await get_single_chat_user_attr_by_username(username, chat_user_table.c.password_hash)
+    async def get_user_password_hash(self, username: str):
+        return await self.get_single_chat_user_attr_by_username(username, self.chat_user_table.c.password_hash)
 
 
 def hash_password(plain_text_password: str) -> str:
--- bot/websocket.py
+++ bot/websocket.py
@@ -1,17 +1,18 @@
 from abc import ABC, abstractmethod
 from fastapi import WebSocket
 from prometheus_client import Gauge
-from sqlalchemy.future import select as sql_select
-from sqlalchemy.sql import desc
+from sqlalchemy import Table, insert
+from sqlalchemy.ext.asyncio import async_sessionmaker as async_pg_session_maker
 from starlette.websockets import WebSocketDisconnect, WebSocketState
+from uuid import UUID, uuid5
+import aiofiles
 import asyncio
-import datetime
 import logging
 
-from bot.api.models import ChatMessage, ChatRequest, ChatResponse
-from bot.db.models import AsyncSessionLocal, ChatSession, ChatRequestReceived, ChatResponseSent
+from bot.api.models import ChatRequest, ChatResponse
 from bot.graph.control_plane import ControlPlane
-from settings.germ_settings import WEBSOCKET_CONNECTION_IDLE_TIMEOUT, WEBSOCKET_SESSION_MONITOR_INTERVAL_SECONDS
+from settings.germ_settings import (DATA_DIR, UUID5_NS,
+                                    WEBSOCKET_CONNECTION_IDLE_TIMEOUT, WEBSOCKET_SESSION_MONITOR_INTERVAL_SECONDS)
 
 logger = logging.getLogger(__name__)
 
@@ -39,31 +40,37 @@ class WebSocketSendEventHandler(ABC):
 
 
 class WebSocketSender:
-    def __init__(self, control_plane: ControlPlane, chat_session_id: int, connection: WebSocket,
+    def __init__(self, chat_message_table: Table, control_plane: ControlPlane, chat_session_id: int,
+                 connection: WebSocket, pg_session_maker: async_pg_session_maker,
                  send_event_handlers: list[WebSocketSendEventHandler] = None):
+        self.chat_message_table = chat_message_table
         self.chat_session_id = chat_session_id
         self.connection = connection
         self.control_plane = control_plane
+        self.pg_session_maker = pg_session_maker
         self.send_event_handlers: list[WebSocketSendEventHandler] = (
             send_event_handlers if send_event_handlers is not None else [])
 
     async def send_chat_response(self, chat_response: ChatResponse):
         await self.connection.send_text(chat_response.model_dump_json())
-        chat_response_sent_id = await new_chat_response_sent(
-            self.chat_session_id, chat_response, chat_request_received_id=None)
+        chat_response_sent_id = await self.new_chat_message_sent(self.chat_session_id, chat_response)
 
         _, time_occurred = await self.control_plane.add_chat_response(chat_response_sent_id)
         await self.control_plane.link_chat_response_to_chat_session(
             chat_response_sent_id, self.chat_session_id, time_occurred)
 
+        handler_tasks = []
         for handler in self.send_event_handlers:
-            _ = asyncio.create_task(
-                handler.on_send(chat_response_sent_id, chat_response, self.chat_session_id))
+            handler_tasks.append(
+                asyncio.create_task(
+                    handler.on_send(chat_response_sent_id, chat_response, self.chat_session_id)
+                )
+            )
+        await asyncio.gather(*handler_tasks)
 
     async def return_chat_response(self, chat_request_received_id: int, chat_response: ChatResponse):
         await self.connection.send_text(chat_response.model_dump_json())
-        chat_response_sent_id = await new_chat_response_sent(
-            self.chat_session_id, chat_response, chat_request_received_id=chat_request_received_id)
+        chat_response_sent_id = await self.new_chat_message_sent(self.chat_session_id, chat_response)
 
         _, time_occurred = await self.control_plane.add_chat_response(chat_response_sent_id)
         await self.control_plane.link_chat_response_to_chat_request(
@@ -71,10 +78,35 @@ class WebSocketSender:
         await self.control_plane.link_chat_response_to_chat_session(
             chat_response_sent_id, self.chat_session_id, time_occurred)
 
+        handler_tasks = []
         for handler in self.send_event_handlers:
-            _ = asyncio.create_task(
-                handler.on_send(chat_response_sent_id, chat_response, self.chat_session_id,
-                                chat_request_received_id=chat_request_received_id))
+            handler_tasks.append(
+                asyncio.create_task(
+                    handler.on_send(chat_response_sent_id, chat_response, self.chat_session_id,
+                                    chat_request_received_id=chat_request_received_id)
+                )
+            )
+        await asyncio.gather(*handler_tasks)
+
+    async def new_chat_message_sent(self, session_id: int, chat_response: ChatResponse) -> int:
+        response_json = chat_response.model_dump_json()
+        async with (self.pg_session_maker() as rdb_session):
+            async with rdb_session.begin():
+                try:
+                    insert_stmt = insert(self.chat_message_table).values(
+                        json_sig=await persist_message_data(response_json),
+                        received=False,
+                        session_id=session_id,
+                    ).returning(self.chat_message_table.c.message_id)
+                    result = await rdb_session.execute(insert_stmt)
+                    await rdb_session.commit()
+                    message_id = result.scalar()
+                    logger.info(f"New message '{message_id}' inserted successfully for session {session_id}")
+                    return message_id
+                except Exception as e:
+                    logger.error(f"Failed to insert new message with session {session_id}: {e}")
+                    await rdb_session.rollback()
+                    raise
 
 
 class WebSocketSessionMonitor(ABC):
@@ -92,11 +124,15 @@ class WebSocketReceiveEventHandler(ABC):
 
 
 class WebSocketConnectionManager:
-    def __init__(self, control_plane: ControlPlane):
+    def __init__(self, chat_message_table: Table, chat_session_table: Table,
+                 control_plane: ControlPlane, pg_session_maker: async_pg_session_maker):
         self.active_connections: dict[int, WebSocket] = {}
+        self.chat_message_table = chat_message_table
+        self.chat_session_table = chat_session_table
         self.control_plane = control_plane
         self.disconnect_event_handlers: list[WebSocketDisconnectEventHandler] = []
         self.monitor_tasks: dict[int, tuple[asyncio.Task, asyncio.Event]] = {}
+        self.pg_session_maker = pg_session_maker
         self.receive_event_handlers: list[WebSocketReceiveEventHandler] = []
         self.send_event_handlers: list[WebSocketSendEventHandler] = []
         self.session_monitors: list[WebSocketSessionMonitor] = []
@@ -125,17 +161,18 @@ class WebSocketConnectionManager:
             await self.disconnect(chat_session_id)
 
     async def connect(self, user_id: int, ws: WebSocket) -> int:
-        chat_session_id = await new_chat_session(user_id)
-        self.active_connections[chat_session_id] = ws
+        session_id = await self.new_chat_session(user_id)
+        self.active_connections[session_id] = ws
 
         # Create and store a cancellation event, and start a monitor task
         cancel_event = asyncio.Event()
-        monitor_task = asyncio.create_task(self.monitor_chat_session(user_id, chat_session_id, ws, cancel_event))
-        self.monitor_tasks[chat_session_id] = (monitor_task, cancel_event)
+        monitor_task = asyncio.create_task(self.monitor_chat_session(user_id, session_id, ws, cancel_event))
+        self.monitor_tasks[session_id] = (monitor_task, cancel_event)
 
         METRIC_CHAT_SESSIONS_IN_PROGRESS.inc()
-        _ = asyncio.create_task(self.control_plane.add_chat_session(chat_session_id))
-        return chat_session_id
+        await self.control_plane.add_chat_session(session_id)
+        await self.control_plane.link_chat_user_to_chat_session(user_id, session_id)
+        return session_id
 
     async def disconnect(self, chat_session_id: int):
         ws = self.active_connections.pop(chat_session_id, None)
@@ -151,9 +188,10 @@ class WebSocketConnectionManager:
             await ws.close()
 
         METRIC_CHAT_SESSIONS_IN_PROGRESS.dec()
-        await update_chat_session_time_stopped(chat_session_id)
+        handler_tasks = []
         for handler in self.disconnect_event_handlers:
-            _ = asyncio.create_task(handler.on_disconnect(chat_session_id))
+            handler_tasks.append(asyncio.create_task(handler.on_disconnect(chat_session_id)))
+        await asyncio.gather(*handler_tasks)
 
     async def disconnect_all(self):
         await asyncio.gather(*[self.disconnect(chat_session_id) for chat_session_id in self.active_connections])
@@ -163,7 +201,7 @@ class WebSocketConnectionManager:
         Runs in the background to do periodic or event-driven checks.
         Closes automatically when cancel_event is set.
         """
-        ws_sender = WebSocketSender(self.control_plane, chat_session_id, ws,
+        ws_sender = WebSocketSender(self.chat_message_table, self.control_plane, chat_session_id, ws, self.pg_session_maker,
                                     send_event_handlers=self.send_event_handlers)
         try:
             while not cancel_event.is_set():
@@ -180,113 +218,70 @@ class WebSocketConnectionManager:
             # Clean-up logic if needed
             pass
 
-    async def receive(self, user_id: int, chat_session_id: int):
-        connection: WebSocket = self.active_connections[chat_session_id]
+    async def new_chat_message_received(self, session_id: int, chat_request: ChatRequest) -> int:
+        request_json = chat_request.model_dump_json()
+        async with (self.pg_session_maker() as rdb_session):
+            async with rdb_session.begin():
+                try:
+                    insert_stmt = insert(self.chat_message_table).values(
+                        json_sig=await persist_message_data(request_json),
+                        received=True,
+                        session_id=session_id,
+                    ).returning(self.chat_message_table.c.message_id)
+                    result = await rdb_session.execute(insert_stmt)
+                    await rdb_session.commit()
+                    message_id = result.scalar()
+                    logger.info(f"New message '{message_id}' inserted successfully for session {session_id}")
+                    return message_id
+                except Exception as e:
+                    logger.error(f"Failed to insert new message with session {session_id}: {e}")
+                    await rdb_session.rollback()
+                    raise
+
+    async def new_chat_session(self, user_id: int) -> int:
+        async with (self.pg_session_maker() as rdb_session):
+            async with rdb_session.begin():
+                try:
+                    insert_stmt = insert(self.chat_session_table).values(
+                        user_id=user_id,
+                    ).returning(self.chat_session_table.c.session_id)
+                    result = await rdb_session.execute(insert_stmt)
+                    await rdb_session.commit()
+                    session_id = result.scalar()
+                    logger.info(f"New session '{session_id}' with user {user_id} inserted successfully")
+                    return session_id
+                except Exception as e:
+                    logger.error(f"Failed to insert new session with user {user_id}: {e}")
+                    await rdb_session.rollback()
+                    raise
+
+    async def receive(self, user_id: int, session_id: int):
+        connection: WebSocket = self.active_connections[session_id]
         chat_request = ChatRequest.model_validate(await connection.receive_json())
 
-        chat_request_received_id = await new_chat_request_received(chat_session_id, chat_request)
-        _, time_occurred = await self.control_plane.add_chat_request(chat_request_received_id)
+        message_id = await self.new_chat_message_received(session_id, chat_request)
+        _, time_occurred = await self.control_plane.add_chat_request(message_id)
         _ = asyncio.create_task(
             self.control_plane.link_chat_request_to_chat_session(
-                chat_request_received_id, chat_session_id, time_occurred))
+                message_id, session_id, time_occurred))
 
-        ws_sender = WebSocketSender(self.control_plane, chat_session_id, connection,
+        ws_sender = WebSocketSender(self.chat_message_table, self.control_plane, session_id, connection,
+                                    self.pg_session_maker,
                                     send_event_handlers=self.send_event_handlers)
+        handler_tasks = []
         for handler in self.receive_event_handlers:
-            _ = asyncio.create_task(
-                handler.on_receive(chat_session_id, chat_request_received_id, chat_request, ws_sender))
+            handler_tasks.append(
+                asyncio.create_task(handler.on_receive(session_id, message_id, chat_request, ws_sender)))
+        await asyncio.gather(*handler_tasks)
 
 
-async def get_chat_session_messages(chat_session_id: int) -> list[ChatMessage]:
-    messages: list[ChatMessage] = []
-    async with (AsyncSessionLocal() as rdb_session):
-        async with rdb_session.begin():
-            response_sent_select_stmt = sql_select(ChatResponseSent).where(
-                ChatResponseSent.chat_session_id == chat_session_id
-            ).order_by(
-                desc(ChatResponseSent.chat_response_sent_id)
-            )
-            response_sent_select_result = await rdb_session.execute(response_sent_select_stmt)
-            response_sent_record = response_sent_select_result.scalars().first()
-            if response_sent_record:
-                if response_sent_record.chat_request_received_id is not None:
-
-                    request_received_select_stmt = sql_select(ChatRequestReceived).join(ChatResponseSent).filter(
-                        ChatRequestReceived.chat_request_received_id == ChatResponseSent.chat_request_received_id,
-                        ChatResponseSent.chat_response_sent_id == response_sent_record.chat_response_sent_id
-                    )
-                    request_received_select_result = await rdb_session.execute(request_received_select_stmt)
-                    request_received_record = request_received_select_result.scalars().one_or_none()
-                    if request_received_record:
-                        for m in ChatRequest.model_validate(request_received_record.chat_request).messages:
-                            messages.append(m)
-                messages.append(
-                    ChatResponse.model_validate(response_sent_record.chat_response))
-    return messages
-
-
-async def new_chat_request_received(chat_session_id: int, chat_request: ChatRequest) -> int:
-    async with (AsyncSessionLocal() as rdb_session):
-        async with rdb_session.begin():
-            rdb_record = ChatRequestReceived(
-                chat_session_id=chat_session_id,
-                chat_request=chat_request.model_dump(),
-                time_received=datetime.datetime.now(datetime.timezone.utc),
-            )
-            rdb_session.add(rdb_record)
-            await rdb_session.commit()
-            return rdb_record.chat_request_received_id
-
-
-async def new_chat_response_sent(chat_session_id: int, chat_response: ChatResponse,
-                           chat_request_received_id: int = None) -> int:
-    async with (AsyncSessionLocal() as rdb_session):
-        async with rdb_session.begin():
-            rdb_record = ChatResponseSent(
-                chat_session_id=chat_session_id,
-                chat_response=chat_response.model_dump(),
-                time_sent=datetime.datetime.now(datetime.timezone.utc),
-            ) if chat_request_received_id is None else ChatResponseSent(
-                chat_session_id=chat_session_id,
-                chat_request_received_id=chat_request_received_id,
-                chat_response=chat_response.model_dump(),
-                time_sent=datetime.datetime.now(datetime.timezone.utc),
-            )
-            rdb_session.add(rdb_record)
-            await rdb_session.commit()
-            return rdb_record.chat_response_sent_id
-
-
-async def new_chat_session(user_id: int) -> int:
-    async with (AsyncSessionLocal() as rdb_session):
-        async with rdb_session.begin():
-            rdb_record = ChatSession(time_started=datetime.datetime.now(datetime.timezone.utc))
-            rdb_session.add(rdb_record)
-            await rdb_session.commit()
-            return rdb_record.chat_session_id
-
-
-async def update_chat_session_is_hidden(chat_session_id: int):
-    async with (AsyncSessionLocal() as rdb_session):
-        async with rdb_session.begin():
-            chat_session_select_stmt = sql_select(ChatSession).where(ChatSession.chat_session_id == chat_session_id)
-            chat_session_select_result = await rdb_session.execute(chat_session_select_stmt)
-            chat_session_record = chat_session_select_result.scalars().first()
-            if chat_session_record:
-                chat_session_record.is_hidden = True
-                await rdb_session.commit()
-            else:
-                logger.error(f"ChatSession.chat_session_id == {chat_session_id} not found")
-
-
-async def update_chat_session_time_stopped(chat_session_id: int):
-    async with (AsyncSessionLocal() as rdb_session):
-        async with rdb_session.begin():
-            chat_session_select_stmt = sql_select(ChatSession).where(ChatSession.chat_session_id == chat_session_id)
-            chat_session_select_result = await rdb_session.execute(chat_session_select_stmt)
-            chat_session_record = chat_session_select_result.scalars().first()
-            if chat_session_record:
-                chat_session_record.time_stopped = datetime.datetime.now(datetime.timezone.utc)
-                await rdb_session.commit()
-            else:
-                logger.error(f"ChatSession.chat_session_id == {chat_session_id} not found")
+async def persist_message_data(json_blob) -> UUID:
+    json_sig = uuid5(UUID5_NS, json_blob)
+    data_filename = f"{DATA_DIR}/messages/{json_sig}.json"
+    try:
+        async with aiofiles.open(data_filename, "w") as f:
+            await f.write(json_blob)
+        return json_sig
+    except Exception as e:
+        logger.error(f"Failed to write {data_filename}: {e}")
+        raise
