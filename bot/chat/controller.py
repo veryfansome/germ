1e26c66083d06adf6acdd633d13289807fff4fe5
fzhu <fzhu@yahooinc.com>
2025-05-02 18:14:48 -0700

    feat: reintegrated emotions

--- bot/chat/controller.py
+++ bot/chat/controller.py
@@ -1,10 +1,13 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from starlette.concurrency import run_in_threadpool
+import aiohttp
 import asyncio
 import logging
 
 from bot.api.models import ChatRequest, ChatResponse
 from bot.graph.control_plane import ControlPlane
+from bot.lang.parsers import extract_markdown_page_elements
+from bot.lang.patterns import naive_sentence_end_pattern
 from bot.websocket import (WebSocketDisconnectEventHandler, WebSocketReceiveEventHandler, WebSocketSendEventHandler,
                            WebSocketSender, WebSocketSessionMonitor)
 from observability.annotations import measure_exec_seconds
@@ -21,16 +24,6 @@ class ChatController(WebSocketDisconnectEventHandler, WebSocketReceiveEventHandl
         self.control_plane = control_plane
         self.remote = remote
 
-    async def get_tf_idf_keywords(self, text: str):
-        documents = [text]
-        # The IDF part means I should retrieve a bunch of random documents from memory and append them to documents.
-        tfidf_matrix = await run_in_threadpool(tf_idf_vectorizer.fit_transform, documents, y=None)
-        feature_names = tf_idf_vectorizer.get_feature_names_out()
-        first_doc_scores = tfidf_matrix[0].toarray().flatten()
-        top_indices = first_doc_scores.argsort()[::-1]
-        top_terms = [feature_names[i] for i in top_indices[:5]]
-        logger.info(f"Top terms: {top_terms}")
-
     async def on_disconnect(self, conversation_id: int):
         pass
 
@@ -38,11 +31,32 @@ class ChatController(WebSocketDisconnectEventHandler, WebSocketReceiveEventHandl
     async def on_receive(self, conversation_id: int, chat_request_received_id: int, chat_request: ChatRequest,
                          ws_sender: WebSocketSender):
         remote_response_task = asyncio.create_task(
+            # Send to LLM
             self.remote.on_receive(conversation_id, chat_request_received_id, chat_request, ws_sender),
         )
-        # TODO: Do stuff.
-        tf_id_keywords = await self.get_tf_idf_keywords(chat_request.messages[-1].content)
+        # Analyze most recent message in chat request
+        code_blocks, text_chunks = await process_markdown_elements(
+            await run_in_threadpool(extract_markdown_page_elements, chat_request.messages[-1].content)
+        )
+        sentence_chunks = []
+        for text_chunk in text_chunks:
+            while text_chunk:
+                # Not always perfect but good enough
+                sentence_end_match = await run_in_threadpool(
+                    naive_sentence_end_pattern.search, text_chunk, 0, len(text_chunk))
+                if sentence_end_match:
+                    sentence_chunks.append(text_chunk[:sentence_end_match.end()])
+                    text_chunk = text_chunk[sentence_end_match.end():].strip()
+                else:
+                    sentence_chunks.append(text_chunk)
+                    text_chunk = ""
 
+        emotion_classification, tf_id_keywords = await asyncio.gather(*[
+            get_emotions_classifications(sentence_chunks),
+            get_tf_idf_keywords(sentence_chunks),
+        ])
+        logger.info(f"emotions: {emotion_classification}")
+        logger.info(f"tf-idf keywords: {tf_id_keywords}")
         await remote_response_task
 
     async def on_send(self,
@@ -50,7 +64,41 @@ class ChatController(WebSocketDisconnectEventHandler, WebSocketReceiveEventHandl
                       chat_response: ChatResponse,
                       conversation_id: int,
                       received_message_id: int = None):
-        tf_id_keywords = await self.get_tf_idf_keywords(chat_response.content)
+        pass
 
     async def on_tick(self, conversation_id: int, ws_sender: WebSocketSender):
         logger.info(f"conversation {conversation_id} is still active")
+
+
+async def get_emotions_classifications(texts: list[str]):
+    async with aiohttp.ClientSession() as session:
+        async with session.post("http://germ-models:9000/text/classification/emotions",
+                                json={"texts": texts}) as response:
+            return await response.json()
+
+
+async def get_tf_idf_keywords(texts: list[str], top: int = 3):
+    documents = texts
+    # The IDF part means I more documents with a broader blend of keywords.
+    tfidf_matrix = await run_in_threadpool(tf_idf_vectorizer.fit_transform, documents, y=None)
+    feature_names = tf_idf_vectorizer.get_feature_names_out()
+
+    keywords = []
+    for text_idx, text in enumerate(texts):
+        doc_scores = tfidf_matrix[text_idx].toarray().flatten()
+        top_indices = doc_scores.argsort()[::-1]
+        keywords.append({"text": text, "keywords": [feature_names[i] for i in top_indices[:top]]})
+    return keywords
+
+
+async def process_markdown_elements(markdown_elements):
+    code_blocks = []
+    text_chunks = []
+    for element in markdown_elements:
+        if element[0] in {"heading", "list_item", "paragraph"}:
+            text_chunks.append(element[1])
+        elif element[0] == "block_code":
+            code_blocks.append({"content": element[2], "language": element[1]})
+        else:
+            logger.info(f"skipped element type: {element[0]}")
+    return code_blocks, text_chunks
