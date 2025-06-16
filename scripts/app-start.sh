9ed6ebbceca7a910d05a29e76b7f2280cd282a41
Fan Zhu <fzhu.admin@icloud.com>
2025-06-15 23:45:54 -0700

    wip: data loader

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-stop_app() {
-  _NOW=$(date +"%Y%m%d-%H%M")
-  # Do something
-}
-
-trap stop_app SIGTERM
-
 source germ_venv/bin/activate
 
 if grep -q 'germ.services.bot.main' <<<"$1"; then
     ./scripts/update-static-files.sh
-    python -m germ.data.wordnet_graph &
 fi
 mkdir -p /var/log/germ
 
