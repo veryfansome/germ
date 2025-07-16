c07b64c10b9ea0ae965616023a8c3a9c3016e5f9
Fan Zhu <fzhu.admin@icloud.com>
2025-07-15 23:50:26 -0700

    chore: merged file with overrides

--- germ/sandbox/token_label_eval.py
+++ germ/sandbox/token_label_eval.py
@@ -2,16 +2,33 @@ import json
 
 
 if __name__ == '__main__':
-    lemmas = {}
-    with open("data/morphemes_a_menadione.json") as fd:
-        for item in  json.load(fd):
-            lemmas[item["word"]] = {"morphemes": item["morphemes"]}
+    #lemmas = {}
+    #for morphemes_file in [
+    #    "data/morphemes_a_menadione.json",
+    #    "data/morphemes_menage_ungainliness.json",
+    #    "data/morphemes_ungainly_zyopterus.json",
+    #]:
+    #    with open(morphemes_file) as fd:
+    #        for item in json.load(fd):
+    #            lemmas[item["word"]] = {"morphemes": item["morphemes"]}
+    #with open("data/morphemes.json", "w") as fd:
+    #    json.dump(lemmas, fd, indent=2)
+    with open("data/morphemes.json") as fd:
+        lemmas = json.load(fd)
 
-    cnt = 0
+    for _ in range(2):  # Make two passes, a few will be missed in the first pass
+        for word, labels in lemmas.items():
+            new_morphemes = []
+            for morpheme in labels["morphemes"]:
+                if morpheme in lemmas and len(lemmas[morpheme]["morphemes"]) > 1:
+                    new_morphemes.extend(lemmas[morpheme]["morphemes"])
+                    continue
+                new_morphemes.append(morpheme)
+            lemmas[word] = {"morphemes": new_morphemes}
+
+    morphemes = set()
     for word, labels in lemmas.items():
-        new_morphemes = []
-        for morpheme in labels["morphemes"]:
-            if morpheme in lemmas and len(lemmas[morpheme]["morphemes"]) > 1:
-                print(morpheme, lemmas[morpheme]["morphemes"])
-                cnt += 1
-    print(cnt)
+        new_morphemes = [m.strip("-") for m in labels["morphemes"]]
+        print(word, new_morphemes)
+        morphemes.update(new_morphemes)
+    print(len(morphemes))
