11de08142abe6f4713baafeac4c8db20b724a9bf
fzhu <fzhu@yahooinc.com>
2025-07-18 18:37:59 -0700

    chore: websocket bug fix

--- scripts/bootstrap-data.sh
+++ scripts/bootstrap-data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
-if ! [ -d data/oewn2024 ]; then
+if [ ! -d data/oewn2024 ]; then
     curl -L \
         -o data/english-wordnet-2024.zip \
         https://github.com/globalwordnet/english-wordnet/releases/download/2024-edition/english-wordnet-2024.zip
@@ -9,17 +9,21 @@ if ! [ -d data/oewn2024 ]; then
     rm data/english-wordnet-2024.zip
 fi
 
-for TBL in categorylinks page; do
-    TBL_DUMP_FILE=data/enwiki-latest-${TBL}.sql.gz
-    if ! [ -f "$TBL_DUMP_FILE" ]; then
-        curl -L \
-            -o "$TBL_DUMP_FILE" \
-            https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-${TBL}.sql.gz
+for TBL in page page_props; do
+    TBL_DUMP_FILE="enwiki-latest-${TBL}.sql"
+    TBL_GZIP_FILE="${TBL_DUMP_FILE}.gz"
+    if [ ! -f "data/${TBL_DUMP_FILE}" ]; then
+        if [ ! -f "data/${TBL_GZIP_FILE}" ]; then
+            curl -L \
+                -o "data/$TBL_GZIP_FILE" \
+                "https://dumps.wikimedia.org/enwiki/latest/${TBL_GZIP_FILE}"
+        fi
+        pigz -d -p 8 "data/$TBL_DUMP_FILE"
     fi
-    pigz -d -p 8 "$TBL_DUMP_FILE"
-    mysql --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" -h germ-mariadb -D germ < "$TBL_DUMP_FILE"
+    #mysql --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" -h germ-mariadb -D germ < "data/$TBL_DUMP_FILE"
 done
 
+set +x
 source germ_venv/bin/activate
 
 mkdir -p /var/log/germ
