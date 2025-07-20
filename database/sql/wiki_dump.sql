5adeba4e1bda17afca20d89c8a98e4e660c7c49b
Fan Zhu <fzhu.admin@icloud.com>
2025-07-19 23:17:57 -0700

    feat: better embs gen and faiss loading

--- database/sql/wiki_dump.sql
+++ database/sql/wiki_dump.sql
@@ -1,23 +1,18 @@
 SET @FA_BADGE := 'wikibase-badge-Q17437796';
 SET @GA_BADGE := 'wikibase-badge-Q17437798';
-SET @SD_PROP := 'wikibase-shortdesc';  -- short-description key
 
 SELECT
-        p.page_title                       AS url_title,
-        p.page_id,
-        CASE pp_badge.pp_propname
-             WHEN @FA_BADGE THEN 'FA'
-             WHEN @GA_BADGE THEN 'GA'
-        END                                AS quality,
-        sd.pp_value                        AS short_description
-FROM        page_props pp_badge  -- badge rows
-INNER JOIN  page       p  ON p.page_id = pp_badge.pp_page
-LEFT  JOIN  page_props sd        -- short-desc rows
-           ON sd.pp_page     = p.page_id
-          AND sd.pp_propname = @SD_PROP
-WHERE       pp_badge.pp_propname IN (@FA_BADGE, @GA_BADGE)
-  AND       p.page_namespace   = 0        -- main space only
-  AND       p.page_is_redirect = 0        -- skip redirects
-ORDER BY    quality DESC,                 -- FA first
-            url_title
+      p.page_id
+    , p.page_title                                  AS url_title
+    , sd.pp_value                                   AS short_description
+    , IF(ppb.pp_propname = @FA_BADGE, 'FA', 'GA')   AS quality
+FROM
+         page               AS p
+    JOIN page_props         AS ppb      ON p.page_id = ppb.pp_page AND p.page_namespace = 0 AND p.page_is_redirect = 0
+    LEFT JOIN page_props    AS sd       ON p.page_id = sd.pp_page  AND sd.pp_propname = 'wikibase-shortdesc'
+WHERE
+        ppb.pp_propname IN (@FA_BADGE, @GA_BADGE)
+ORDER BY
+      quality DESC
+    , url_title
 ;
